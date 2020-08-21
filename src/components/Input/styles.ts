84d33a2be81acdc0bfcc54bb73fd8ac2a0166ab6
viniciusPC <viniciuscarneirodasilva@gmail.com>
2020-08-20 23:46:53 -0300

    SingUp finalizado

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 export const Container = styled.div<ContainerProps>`
   background: #232129;
@@ -28,7 +30,11 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
-
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
   ${props =>
     props.isFocused &&
     css`
@@ -36,12 +42,24 @@ export const Container = styled.div<ContainerProps>`
       border-color: #ff9000;
     `}
 
+
   svg {
     margin-right: 16px;
-    ${props =>
-      props.isFilled &&
-      css`
-        color: #ff9000;
-      `}
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
